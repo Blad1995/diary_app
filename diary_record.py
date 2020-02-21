743af89db39aa3c3e56b2f179527e35e1c425753
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-02-21 21:49:20 +0100

    Update diary_record.py
    - Added title to DiaryRecord
    - Created funcion update on DiaryRecord

--- diary_record.py
+++ diary_record.py
@@ -2,10 +2,34 @@ from datetime import datetime
 
 
 class DiaryRecord:
-    def __init__(self, date, text, date_of_creation=None):
+    def __init__(self, date, title, text, date_of_creation=None):
         self.date = date
+        self.title = title
         self.text = text
         self.date_of_creation = date_of_creation if not date_of_creation else datetime.now()
         self.list_of_alteration_dates = []
         # self.history_of_changes = {}
-        self.list_of_media = []
+        # self.list_of_media = []
+
+    def update(self, date: datetime = None, text: str = None, title: str = None, alteration_date: datetime = None):
+        """
+        Update existing DiaryRecord. Raise TypeError exception if the provided parameters type are not suitable.
+        :param date: New date of the record. If None, then no changes in date happen.
+        :param text: New text of the record. If None, then no changes in text happen.
+        :param title: New title of the record. If None, then no changes in title happen.
+        :param alteration_date: Date of the change in record. Current time used if None alternation_date provided
+        :return: Nothing.
+        """
+        # check proper data types
+        if type(text) is not str:
+            raise TypeError(f"Parameter provided for 'text' is not suitable. Text should be 'string'. Given parameter is: '{type(text)}'")
+        elif type(title) is not str:
+            raise TypeError(f"Parameter provided for 'title' is not suitable. Title should be 'string'. Given parameter is: '{type(title)}'")
+        elif type(date) is not datetime:
+            raise TypeError(f"Parameter provided for 'date' is not suitable. Date should be 'datetime'. Given parameter is: '{type(date)}'")
+
+        self.title = self.title if title is None else title
+        self.text = self.text if text is None else text
+        self.date = self.date if date is None else date
+        self.list_of_alteration_dates.append(datetime.now() if alteration_date is None else alteration_date)
+        
