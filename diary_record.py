eea47c019d3f33382412e43abc593958de3579b5
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-02-21 21:23:52 +0100

    DiaryRecord init
    - Added initialization of DiaryRecord class

--- diary_record.py
+++ diary_record.py
@@ -1,3 +1,11 @@
+from datetime import datetime
+
+
 class DiaryRecord:
-    # TODO
-    pass
+    def __init__(self, date, text, date_of_creation=None):
+        self.date = date
+        self.text = text
+        self.date_of_creation = date_of_creation if not date_of_creation else datetime.now()
+        self.list_of_alteration_dates = []
+        # self.history_of_changes = {}
+        self.list_of_media = []
