32d2c7bf5ff2054118c7e03f211c9dd7a67f5b4d
Blad1995 <Ondra.Divina@seznam.cz>
2020-02-24 11:26:40 +0100

    OwnerInfo setters&getters
    - Added setters and getters for all variables of class OwnerInfo

--- diary_owner_info.py
+++ diary_owner_info.py
@@ -6,6 +6,53 @@ class OwnerInfo:
         self.name = name
         self.photo = photo
         self.email = email
-        self.password = password
+        # for develompent purposes string --> TODO more secure solution
+        # so far it's marked as private
+        self.__password = password
+        # should be constant after creation
         self.date_joined = date_joined if date_joined else datetime.now()
         self.last_active = self.date_joined
+
+    # Name setters & getters
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, name):
+        self.name = name
+
+    # Photo setters & getters
+    @property
+    def photo(self):
+        return self.photo
+
+    @photo.setter
+    def photo(self, photo):
+        self.photo = photo
+
+    # email setters & getters
+    @property
+    def email(self):
+        return self.email
+
+    @email.setter
+    def email(self, email):
+        self.email = email
+
+    # password setters & getters
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, password):
+        self.__password = password
+
+    @property
+    def last_active(self):
+        return self.last_active
+
+    @last_active.setter
+    def last_active(self, last_active):
+        self.last_active = last_active
