d5830a7f2d1a07470a727a409f6f928137c40524
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-03-04 23:18:13 +0100

    First_use
    - First_use config value added
    - Data loaded only when first_use false

--- diary_app.py
+++ diary_app.py
@@ -14,7 +14,7 @@ class Diary:
     def __init__(self):
         self.owners = []
         with open("config.yml", mode="r") as f:
-            self.cfg = yaml.load(f)
+            self.cfg = yaml.safe_load(f)
         if not self.cfg:
             raise RuntimeError("Cannot find config file.")
 
@@ -31,6 +31,7 @@ class Diary:
         self.active_owner = None
 
     def save_data_to_disc(self):
+        set_config_first_use_false()
         owners_names = [o.name for o in self.owners]
         # Saves every Owner separately
         for o_index, name in enumerate(owners_names):
@@ -48,7 +49,7 @@ class Diary:
             log.error(datetime.now().strftime("%d.%m.%Y-%H:%M:%S - ") + "Cannot locate 'diary:owner_name_file_name' in config file")
             raise RuntimeError("Config file has been corrupted. 'diary:owner_name_file_name' missing.")
 
-        try:
+        try:    
             # Saves owners name to file
             with open(owner_path, mode="wb") as f:
                 pickle.dump(obj=owners_names, file=f, protocol=pickle.HIGHEST_PROTOCOL)
@@ -76,7 +77,7 @@ class Diary:
             raise RuntimeError(f"Can't load the data from {owner_path}.")
 
         # load all data about owners
-        # TODO do budoucna možná bude potřeba předělat na samostatné načítání po jednom Ownerovi -->
+        # FIXME do budoucna možná bude potřeba předělat na samostatné načítání po jednom Ownerovi -->
         #  paměťové nároky na načtení všech dat vs jednoho Ownera
         for o_index, name in enumerate(owners_names):
             owner_file_path = self.dir_path + name + ".Ddata"
@@ -88,12 +89,22 @@ class Diary:
                 log.error(datetime.now().strftime("%d.%m.%Y-%H:%M:%S - ") + f"Error writing to {owner_path} file. {e}")
                 raise RuntimeError(f"Can't open the file {owner_file_path} to save data.")
 
+    def set_config_first_use_false(self):
+        #No exception should occur. This property of cfg was already addressed.
+        self.cfg["general"]["first_use"] = False
+        try:
+            with open("config.yml", "wb") as f:
+                yaml.dump()
 
 if __name__ == "__main__":
     main_app = Diary()
-    main_app.owners.append(Owner(name="Ondra Divina", password="Heslo století"))
+    if main_app.cfg["general"]["first_use"]:
+        print("First use was used")
+        main_app.owners.append(Owner(name="Ondra Divina", password="Heslo století"))
 
-    main_app.save_data_to_disc()
+        main_app.save_data_to_disc()
+    else:
+        main_app.load_data_from_disc()
     print(main_app)
 
 
