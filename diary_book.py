f96bdf7b2da36c3c1a972ec2b72d28e3ab67e20b
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-03-05 21:48:05 +0100

    Initial correction of diary_book.py code
    - Default values in __init__ added
    - Whitespaces trimmed
    - added self for list_of_records

--- diary_book.py
+++ diary_book.py
@@ -1,8 +1,9 @@
 from datetime import date
 
+
 class Diary:
-    
-    def __init__(self, title: str, date_of_creation: str, list_of_records: dict, bio: str):
+
+    def __init__(self, title: str, date_of_creation: str = None, list_of_records: dict = None, bio: str = None):
         self.title = title
         self.date_of_creation = date_of_creation if date_of_creation else date.today().strftime("%d. %m. %Y")
         self.list_of_records = list_of_records if list_of_records else []
@@ -12,19 +13,15 @@ class Diary:
     def delete_record(self, record_id: int):
         for record in self.list_of_records:
             if record['id'] == record_id:
-                self.list_of_records.pop
-
-
+                self.list_of_records.pop()
 
     def export(self, destination):
-        with open(f'{self.title.lower()}_{date_of_creation}.txt','w') as f:
+        with open(f'{self.title.lower()}_{self.date_of_creation}.txt', 'w') as f:
             payload = str(
                 {
                     'title': self.title,
                     'date_of_creation': self.date_of_creation,
                     'bio': self.bio,
                     'records': self.list_of_records
-            })
+                    })
             f.write(payload)
- 
-
