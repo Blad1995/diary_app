0d7706f5de94a0fd950f0ceb29d609dc4befaa85
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-03-16 12:47:35 +0100

    Rework dict_of_diary_records
    - list change to dict_of_diary_records
    - delete_record alternated
    - create_record + update_record templates added

--- diary_book.py
+++ diary_book.py
@@ -9,12 +9,12 @@ from config import DiaryConfig
 class Diary:
     cfg = DiaryConfig
 
-    def __init__(self, title: str, date_of_creation: datetime = None, list_of_records: dict = None, bio: str = None):
+    def __init__(self, title: str, date_of_creation: datetime = None, dict_of_records: dict = None, bio: str = None):
         self.title = title
         self.date_of_creation = date_of_creation if date_of_creation else datetime.today()
-        self.list_of_records = list_of_records if list_of_records else []
+        self.dict_of_records = dict_of_records if dict_of_records else {}
         self.bio = bio
-        self.list_of_removed_records = []
+        self.dict_of_removed_records = {}
         log.info(datetime.now().strftime(f"{Diary.cfg.log_time_format} - ") + f"Diary was created {str(self)}")
 
     def __str__(self):
@@ -23,9 +23,10 @@ class Diary:
         return string
 
     def delete_record(self, record_id: int):
-        for record in self.list_of_records:
-            if record['id'] == record_id:
-                self.list_of_records.pop()
+        if self.dict_of_records.get(record_id, default=None):
+            self.dict_of_removed_records.update(self.dict_of_records.pop(record_id))
+        else:
+            log.warning(datetime.now().strftime(f"{Diary.cfg.log_time_format} - ") + f"No such record with id = {record_id} in diary {str(self)}")
 
     def export_to_txt(self, destination: str):
         if not os.path.isdir(destination):
@@ -38,9 +39,18 @@ class Diary:
                         'title': self.title,
                         'date_of_creation': self.date_of_creation,
                         'bio': self.bio,
-                        'records': self.list_of_records
+                        # TODO test if this str conversion works
+                        'records': self.dict_of_records
                         })
                 f.write(payload)
         except IOError as e:
             log.error(datetime.now().strftime(f"{Diary.cfg.log_time_format} - ") + f"Error writing to {file_path}. {e}")
             raise e
+
+    def create_record(self):
+        # TODO create_record
+        pass
+
+    def update_record(self):
+        # TODO update
+        pass
