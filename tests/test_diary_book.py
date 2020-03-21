44c7976a23edcfa42a534ce272aabbac597e568f
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-03-21 15:42:52 +0100

    test_export_to_txt
    - init of test module
    - load of config
    - test if file is created by export
    - new file for test export to .gitignore

--- tests/test_diary_book.py
+++ tests/test_diary_book.py
@@ -1,11 +1,17 @@
+from datetime import datetime
+from os import path
 from unittest import TestCase
+
+from config import DiaryConfig
+from diary_book import Diary
 
 
 class TestDiary(TestCase):
-    def test_delete_record(self):
-        self.fail()
+    def __init__(self, *args, **kwargs):
+        super(TestDiary, self).__init__(*args, **kwargs)
+        DiaryConfig.load("../")
 
-    def test_export_to_txt(self):
+    def test_delete_record(self):
         self.fail()
 
     def test_create_record(self):
@@ -13,3 +19,15 @@ class TestDiary(TestCase):
 
     def test_update_record(self):
         self.fail()
+
+    def test_export_to_txt(self):
+        # TEST 1 - empty Diary
+        test_diary = Diary(title="my title", bio="Můj první deníček")
+        test_diary.export_to_txt("export")
+        self.assertTrue(path.isfile(f"export/{test_diary.title.lower()}_{test_diary.date_of_creation.strftime(test_diary.cfg.date_format)}.txt"))
+
+        # TEST 2 - Diary with record
+        test_diary = Diary(title="my title", bio="Můj první deníček")
+        test_diary.create_record(date_of_record=datetime(2000, 12, 1), title="myTitle", text="whatever\nsomething")
+        test_diary.export_to_txt("export")
+        self.assertTrue(path.isfile(f"export/{test_diary.title.lower()}_{test_diary.date_of_creation.strftime(test_diary.cfg.date_format)}.txt"))
