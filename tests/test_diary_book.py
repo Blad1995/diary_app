3b9e28fb990026a57d0ff6feeac6d286d8bb7161
Ondřej Divina <ondra.divina@seznam.cz>
2020-03-21 14:44:26 +0100

    test_diary_book created

---
+++ tests/test_diary_book.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestDiary(TestCase):
+    def test_delete_record(self):
+        self.fail()
+
+    def test_export_to_txt(self):
+        self.fail()
+
+    def test_create_record(self):
+        self.fail()
+
+    def test_update_record(self):
+        self.fail()
